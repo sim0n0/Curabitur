73e2aa1627361f16039df0cbb8c2452448e4d7fe
badreloifi <31614645+badreloifi@users.noreply.github.com>
2020-06-01 18:40:03 +0200

    synchronisation des methodes entre class et ajout de commentaire explicatifs

--- src/com/company/TextListener.java
+++ src/com/company/TextListener.java
@@ -27,40 +27,45 @@ import java.util.Arrays;
 
 /* ON VERIFIE SI LES CHAMPS NE SONT PAS VIDE */
   public class TextListener implements DocumentListener{
-    JTextField jtf1;
-    JTextField jtf2;
-    JTextField jtf3;
-    JButton jcbtn;
-
-    public TextListener(JTextField jtf1, JTextField jtf2, JTextField jtf3, JButton jcbtn){
-      this.jtf1 = jtf1;
-      this.jtf2 = jtf2;
-      this.jtf3 = jtf3;
-      this.jcbtn = jcbtn;
+    JTextField chdt1;
+    JTextField chdt2;
+    JTextField chdt3;
+    JButton btn;
+	
+	//méthode des lectures des champs de texte et bouton.
+    public TextListener(JTextField chdt1, JTextField chdt2, JTextField chdt3, JButton btn){
+      this.chdt1 = chdt1;
+      this.chdt2 = chdt2;
+      this.chdt3 = chdt3;
+      this.btn = btn;
+    }
+	
+	//méthode de modification des champs de texte et suppression des espacements.
+	public void removeUpdate(DocumentEvent e) {
+      if(chdt1.getText().trim().equals("") ||
+          chdt2.getText().trim().equals("") ||
+          chdt3.getText().trim().equals("")
+          ){
+        btn.setEnabled(false);
+      }else{
+        btn.setEnabled(true);
+      }
+    }
+	
+	//méthode d'insertion des modification des champs de texte.
+	public void insertUpdate(DocumentEvent e) {
+      if(chdt1.getText().trim().equals("") ||
+          chdt2.getText().trim().equals("") ||
+          chdt3.getText().trim().equals("")
+          ){
+        btn.setEnabled(false);
+      }else{
+        btn.setEnabled(true);
+      }
     }
 
     public void changedUpdate(DocumentEvent e) {}
 
-    public void removeUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
-    public void insertUpdate(DocumentEvent e) {
-      if(jtf1.getText().trim().equals("") ||
-          jtf2.getText().trim().equals("") ||
-          jtf3.getText().trim().equals("")
-          ){
-        jcbtn.setEnabled(false);
-      }else{
-        jcbtn.setEnabled(true);
-      }
-    }
 
   }
 
