7947a79c811056fa5ce9e7946e7e43b33223aff7
Aravinth SHANTHIRABALAN <aravinth.shanthir@gmail.com>
2020-06-01 13:57:02 +0200

    Correction d'erreur et gestion des clients/server

--- src/com/company/CurabiturServer.java
+++ src/com/company/CurabiturServer.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.awt.Color;
+import java.util.Scanner;
 
 /**
  * Classe du serveur
@@ -34,99 +35,120 @@ public class CurabiturServer {
         this.port = port;
         this.clients = new ArrayList<User>();
     }
-	
-	public void removeUser(User user)
-	{
-		this.clients.remove(user);
-	}
-	
-	public void addUser(User user)
-	{
-		this.clients.add(user);
-	}
-	
-	public void showUser()
-	{
-		
-		if(clients.size()==0)
-		{
-			System.out.println("Le serveur est vide");
-		}
-		else
-		{
-			String results=" ";
-			for(int i =0; i < clients.size(); i++)
-			{
-				results+=clients.get(i).getNickname();
-				results+="\n";
-			}
-			System.out.println("Voici la liste des personnes connectés au serveur : \n" );
-		}
-	}
 
-    
-	
-	
-	
-	
-	
-	public void broadcastMessages(String msg, User userSender) {
-    for (User client : this.clients) {
-      client.getOutStream().println(
-          userSender.toString() + "<span>: " + msg+"</span>");
+    /**
+     * Affiche la liste des clients inscrits dans le serveur, si la liste est vide affiche un message
+     */
+    public void removeUser(User user)
+    {
+        this.clients.remove(user);
     }
-  }
 
-  // send list of clients to all Users
-  public void broadcastAllUsers(){
-    for (User client : this.clients) {
-      client.getOutStream().println(this.clients);
+    /**
+     * Ajoute un user donné en paramètre de la liste des clients
+     * @param user
+     */
+    public void addUser(User user)
+    {
+        this.clients.add(user);
     }
-  }
 
-  // send message to a User (String)
-  public void sendMessageToUser(String msg, User userSender, String user){
-    boolean find = false;
-    for (User client : this.clients) {
-      if (client.getNickname().equals(user) && client != userSender) {
-        find = true;
-        userSender.getOutStream().println(userSender.toString() + " -> " + client.toString() +": " + msg);
-        client.getOutStream().println(
-            "(<b>Private</b>)" + userSender.toString() + "<span>: " + msg+"</span>");
-      }
+    /**
+     * Affiche la liste des clients inscrits dans le serveur, si la liste est vide affiche un message
+     */
+    public void showUser()
+    {
+
+        if(clients.size()==0)
+        {
+            System.out.println("Le serveur est vide");
+        }
+        else
+        {
+            String results=" ";
+            for(int i =0; i < clients.size(); i++)
+            {
+                results+=clients.get(i).getNickname();
+                results+="\n";
+            }
+            System.out.println("Voici la liste des personnes connectés au serveur : \n" );
+        }
     }
-    if (!find) {
-      userSender.getOutStream().println(userSender.toString() + " -> (<b>no one!</b>): " + msg);
-    }
-  }
-	
-	
-	
-	
-
-
 
     /**
      * Boucle de lancement du serveur
      * @throws IOException
      */
     public void run() throws IOException {
-	try {
-		server = new ServerSocket(port);
-	} catch (IOException e) {
-		System.out.println("Erreur à la création :\n"+ e);	
-		server.close();
-	}
-	System.out.println("CurabiturServer a ouvert et écoute le port "+port);
-  
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Erreur à la création :\n"+ e);
+            server.close();
+        }
+        System.out.println("CurabiturServer a ouvert et écoute le port "+port);
+
 
         while(true)
         {
-			showUser();
             // Ajout client
             Socket client = server.accept();
+            showUser();
+
+            // 
+            String nickname = (new Scanner ( client.getInputStream() )).nextLine();
+
+//Création et ajout du user depuis client
+            User newUser = new User(client, nickname);
+            this.clients.add(newUser);
+
+            new Thread(new UserHandler(this, newUser)).start();
+        }
+
+
+
+
+    }
+
+
+    public void broadcastMessages(String msg, User userSender) {
+        for (User client : this.clients) {
+            client.getOutStream().println(
+                    userSender.toString() + "<span>: " + msg+"</span>");
         }
     }
+
+    // send list of clients to all Users
+    public void broadcastAllUsers(){
+        for (User client : this.clients) {
+            client.getOutStream().println(this.clients);
+        }
+    }
+
+    // send message to a User (String)
+    public void sendMessageToUser(String msg, User userSender, String user){
+        boolean find = false;
+        for (User client : this.clients) {
+            if (client.getNickname().equals(user) && client != userSender) {
+                find = true;
+                userSender.getOutStream().println(userSender.toString() + " -> " + client.toString() +": " + msg);
+                client.getOutStream().println(
+                        "(<b>Private</b>)" + userSender.toString() + "<span>: " + msg+"</span>");
+            }
+        }
+        if (!find) {
+            userSender.getOutStream().println(userSender.toString() + " -> (<b>no one!</b>): " + msg);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) throws IOException
     {
--- src/com/company/UserHandler.java
+++ src/com/company/UserHandler.java
@@ -1,68 +1,69 @@
-package com.company;
+
 import java.lang.System;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.awt.Color;
 
 public class UserHandler implements Runnable {
 
-  private CurabiturServer server;
-  private User user;
+    private CurabiturServer server;
+    private User user;
 
-  public UserHandler(CurabiturServer server, User user) {
-    this.server = server;
-    this.user = user;
-    this.server.broadcastAllUsers();
-  }
-
-  public void run() {
-    String message;
-
-    // when there is a new message, broadcast to all
-    Scanner sc = new Scanner(this.user.getInputStream());
-    while (sc.hasNextLine()) {
-      message = sc.nextLine();
-
-      // smiley
-      message = message.replace(":)", "<img src='http://4.bp.blogspot.com/-ZgtYQpXq0Yo/UZEDl_PJLhI/AAAAAAAADnk/2pgkDG-nlGs/s1600/facebook-smiley-face-for-comments.png'>");
-      message = message.replace(":D", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
-      message = message.replace(":d", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
-      message = message.replace(":(", "<img src='http://2.bp.blogspot.com/-rnfZUujszZI/UZEFYJ269-I/AAAAAAAADnw/BbB-v_QWo1w/s1600/facebook-frown-emoticon.png'>");
-      message = message.replace("-_-", "<img src='http://3.bp.blogspot.com/-wn2wPLAukW8/U1vy7Ol5aEI/AAAAAAAAGq0/f7C6-otIDY0/s1600/squinting-emoticon.png'>");
-      message = message.replace(";)", "<img src='http://1.bp.blogspot.com/-lX5leyrnSb4/Tv5TjIVEKfI/AAAAAAAAAi0/GR6QxObL5kM/s400/wink%2Bemoticon.png'>");
-      message = message.replace(":P", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
-      message = message.replace(":p", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
-      message = message.replace(":o", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
-      message = message.replace(":O", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
-
-      // Gestion des messages private
-      if (message.charAt(0) == '@'){
-        if(message.contains(" ")){
-          System.out.println("private msg : " + message);
-          int firstSpace = message.indexOf(" ");
-          String userPrivate= message.substring(1, firstSpace);
-          server.sendMessageToUser(
-              message.substring(
-                firstSpace+1, message.length()
-                ), user, userPrivate
-              );
-        }
-
-      // Gestion du changement
-      }else if (message.charAt(0) == '#'){
-        user.changeColor(message);
-        // update color for all other users
+    public UserHandler(CurabiturServer server, User user) {
+        this.server = server;
+        this.user = user;
         this.server.broadcastAllUsers();
-      }else{
-        // update user list
-        server.broadcastMessages(message, user);
-      }
     }
-    // end of Thread
-    server.removeUser(user);
-    this.server.broadcastAllUsers();
-    sc.close();
-  }
+
+    public void run() {
+        String message;
+
+        // when there is a new message, broadcast to all
+        Scanner sc = new Scanner(this.user.getInputStream());
+        while (sc.hasNextLine()) {
+            message = sc.nextLine();
+
+            // smiley
+            message = message.replace(":)", "<img src='http://4.bp.blogspot.com/-ZgtYQpXq0Yo/UZEDl_PJLhI/AAAAAAAADnk/2pgkDG-nlGs/s1600/facebook-smiley-face-for-comments.png'>");
+            message = message.replace(":D", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
+            message = message.replace(":d", "<img src='http://2.bp.blogspot.com/-OsnLCK0vg6Y/UZD8pZha0NI/AAAAAAAADnY/sViYKsYof-w/s1600/big-smile-emoticon-for-facebook.png'>");
+            message = message.replace(":(", "<img src='http://2.bp.blogspot.com/-rnfZUujszZI/UZEFYJ269-I/AAAAAAAADnw/BbB-v_QWo1w/s1600/facebook-frown-emoticon.png'>");
+            message = message.replace("-_-", "<img src='http://3.bp.blogspot.com/-wn2wPLAukW8/U1vy7Ol5aEI/AAAAAAAAGq0/f7C6-otIDY0/s1600/squinting-emoticon.png'>");
+            message = message.replace(";)", "<img src='http://1.bp.blogspot.com/-lX5leyrnSb4/Tv5TjIVEKfI/AAAAAAAAAi0/GR6QxObL5kM/s400/wink%2Bemoticon.png'>");
+            message = message.replace(":P", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
+            message = message.replace(":p", "<img src='http://4.bp.blogspot.com/-bTF2qiAqvi0/UZCuIO7xbOI/AAAAAAAADnI/GVx0hhhmM40/s1600/facebook-tongue-out-emoticon.png'>");
+            message = message.replace(":o", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
+            message = message.replace(":O", "<img src='http://1.bp.blogspot.com/-MB8OSM9zcmM/TvitChHcRRI/AAAAAAAAAiE/kdA6RbnbzFU/s400/surprised%2Bemoticon.png'>");
+
+            // Gestion des messages private
+            if (message.charAt(0) == '@'){
+                if(message.contains(" ")){
+                    System.out.println("private msg : " + message);
+                    int firstSpace = message.indexOf(" ");
+                    String userPrivate= message.substring(1, firstSpace);
+                    server.sendMessageToUser(
+                            message.substring(
+                                    firstSpace+1, message.length()
+                            ), user, userPrivate
+                    );
+                }
+
+                // Gestion du changement
+            }else if (message.charAt(0) == '#'){
+                user.changeColor(message);
+                // update color for all other users
+                this.server.broadcastAllUsers();
+            }else{
+                // update user list
+                server.broadcastMessages(message, user);
+            }
+        }
+        // end of Thread
+        server.removeUser(user);
+        this.server.broadcastAllUsers();
+        sc.close();
+    }
 }
